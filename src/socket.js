d6ae146f0142b7e2a7f752b3ee6fb00176d667f2
고은서 <gogi0827@gmail.com>
2025-06-10 06:54:25 +0900

    서버수정

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:4000"); // 명시적으로 WebSocket 사용
+const socket = io("https://server-uh7v.onrender.com"); // 명시적으로 WebSocket 사용
 export default socket;
